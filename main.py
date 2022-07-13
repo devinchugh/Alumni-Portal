db8e67dfb5445982ab1776d647011aaf76eeeebb
Devin <devinchugh.dc@gmail.com>
2022-07-13 15:02:22 +0530

    Making the necessary changes for deploying

--- main.py
+++ main.py
@@ -246,4 +246,4 @@ if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0")
